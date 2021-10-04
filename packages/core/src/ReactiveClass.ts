89a2221e40a7c3d8167d01675864cc871235d5d1
Sijia Wang <sijiaoh@outlook.com>
2021-10-05 02:31:04 +0900

    Fix lint error.

--- packages/core/src/ReactiveClass.ts
+++ packages/core/src/ReactiveClass.ts
@@ -1,6 +1,7 @@
 export class ReactiveClass {
   constructor() {
     return new Proxy(this, {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       set: (target: any, p, value) => {
         if (target[p] === value) return true;
         target[p] = value;
