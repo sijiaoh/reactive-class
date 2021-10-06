19792713b1bebef742d58905109bceeba867d1fa
Sijia Wang <sijiaoh@outlook.com>
2021-10-06 17:08:40 +0900

    Add onChange propagation.

--- packages/core/src/ReactiveClass.ts
+++ packages/core/src/ReactiveClass.ts
@@ -1,15 +1,53 @@
 export class ReactiveClass {
+  public static __onChange?: (instance: ReactiveClass) => void;
+  private static instances: ReactiveClass[] = [];
+
+  private __self: ReactiveClass;
+  private __parents: ReactiveClass[] = [];
+
   constructor() {
-    return new Proxy(this, {
+    const instance = new Proxy(this, {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       set: (target: any, p, value) => {
-        if (target[p] === value) return true;
+        const prop = target[p];
+        if (prop === value) return true;
+
         target[p] = value;
-        this.__onChange();
+
+        if (!p.toString().startsWith('__')) {
+          if (prop instanceof ReactiveClass) {
+            prop.__parents = prop.__parents.filter(parent => parent === this);
+          }
+          if (value instanceof ReactiveClass) {
+            value.__parents.push(this);
+          }
+
+          this.__onChange();
+          this.__parents.forEach(parent => parent.__onChange());
+        }
+
         return true;
       },
     });
+
+    this.__self = instance;
+    ReactiveClass.instances.push(instance);
+
+    return instance;
   }
 
-  private __onChange = () => {};
+  destroy = () => {
+    ReactiveClass.instances = ReactiveClass.instances.filter(
+      instance => instance !== this.__self
+    );
+    ReactiveClass.instances.forEach(instance => {
+      instance.__parents = instance.__parents.filter(
+        parent => parent !== this.__self
+      );
+    });
+  };
+
+  private __onChange = () => {
+    ReactiveClass.__onChange?.(this.__self);
+  };
 }
